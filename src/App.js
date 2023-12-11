6ec08d06e2cee8c1e62086ebd71313f3ddc1c004
Olesya Malaya <olesya.malaya@karazin.ua>
2023-12-11 08:04:05 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
   return (
-    <div className="App">
-     Test
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/router.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import { ROUTES } from "./constants/constants";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MenuPage from "./pages/MenuPage/MenuPage";
+import ReservationPage from "./pages/ReservationPage/ReservationPage";
+import OrderPage from "./pages/OrderPage/OrderPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+
+export const router = createBrowserRouter([
+  {
+    path: ROUTES.HOME,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: ROUTES.ABOUT,
+        element: <AboutPage />,
+      },
+      {
+        path: ROUTES.MENU,
+        element: <MenuPage />,
+      },
+      {
+        path: ROUTES.RESERVATION,
+        element: <ReservationPage />,
+      },
+      {
+        path: ROUTES.ORDER,
+        element: <OrderPage />,
+      },
+      {
+        path: ROUTES.LOGIN,
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
