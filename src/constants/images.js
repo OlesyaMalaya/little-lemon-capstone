5df53fadd562e0dc4f3352765a52080ec517e44a
Olesya Malaya <olesya.malaya@karazin.ua>
2023-12-16 10:17:18 +0200

    optimized image sizes for performance

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,19 +1,20 @@
-import eric from "../assets/Eric.png";
+import eric from "../assets/Eric.webp";
 import alice from "../assets/Alice.png";
 import jiovanni from "../assets/Jiovanni.png";
-import greekSalad from "../assets/greek salad.png";
-import bruschetta from "../assets/bruschetta.png";
+import greekSalad from "../assets/greek salad.webp";
+import bruschetta from "../assets/bruschetta.webp";
 import lemonDessert from "../assets/lemon dessert.png";
-import pasta from "../assets/pasta.png";
-import seabass from "../assets/seabass.png";
-import coffee from "../assets/coffee.png";
-import tea from "../assets/tea.png";
-import lemonade from "../assets/lemonade.png";
-import owners1 from "../assets/owners1.png";
-import owners2 from "../assets/owners2.png";
-import chef from "../assets/restaurant_chef.png";
-import restaurant from "../assets/restaurant.png";
-import plate from "../assets/plate.png";
+import pasta from "../assets/pasta.webp";
+import seabass from "../assets/seabass.webp";
+import coffee from "../assets/coffee.webp";
+
+import tea from "../assets/tea.webp";
+import lemonade from "../assets/lemonade.webp";
+import owners1 from "../assets/owners1.webp";
+import owners2 from "../assets/owners2.webp";
+import chef from "../assets/restaurant_chef.webp";
+import restaurant from "../assets/restaurant.webp";
+import plate from "../assets/plate.webp";
 import star from "../assets/star.png";
 
 
