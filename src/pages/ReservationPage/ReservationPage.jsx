6b656af7845cc84842e617d8074748a367c45770
Olesya Malaya <olesya.malaya@karazin.ua>
2023-12-16 12:12:33 +0200

    added react-hook-form

--- src/pages/ReservationPage/ReservationPage.jsx
+++ src/pages/ReservationPage/ReservationPage.jsx
@@ -1,9 +1,23 @@
-
-
+import BookingForm from "../../components/BookingForm/BookingForm";
+import { restaurant } from "../../constants/images";
+import Hero from "../../components/Hero/Hero";
+const reservationPageHero = {
+  heading: "Book a table",
+  subheading: "at Little Lemon",
+  description:
+    "Confirm your reservation, and you're all set! You can tailor your experience by specifying any special requests.",
+  cta_link: null,
+  cta_text: "",
+  image_src: restaurant,
+  image_text: "Restaurant view",
+};
 const ReservationPage = () => {
   return (
-    <div>ReservationPage</div>
-  )
-}
+    <>
+      <Hero hero={reservationPageHero} />
+      <BookingForm />
+    </>
+  );
+};
 
-export default ReservationPage
+export default ReservationPage;
