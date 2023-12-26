41606e5798aae23e0afd09ee8b6c0519bfb7f451
Olesya Malaya <olesya.malaya@karazin.ua>
2023-12-26 12:20:15 +0200

    fixed a typo

--- src/data/dates.js
+++ src/data/dates.js
@@ -3,5 +3,5 @@ tomorrow.setDate(tomorrow.getDate() + 1);
 export const initialDate = tomorrow.toISOString().slice(0, 10);
 
 const date = new Date();
-date.setDate(date.getDate() + 1);
+date.setDate(date.getDate() + 4);
 export const mockDate = date.toISOString().slice(0, 10);
